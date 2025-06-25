2f91897b5a1c7913720973ff5b34aa6f41130074
baptiste gresse <baptiste5gresse@gmail.com>
2025-06-25 17:22:22 +0200

    Refactor EditProfileForm layout and styling for improved user experience, including adjustments to form structure, padding, and element alignment. Ensure consistent design for profile editing fields and buttons.

--- src/app/dashboard/edit-profile/EditProfileForm.tsx
+++ src/app/dashboard/edit-profile/EditProfileForm.tsx
@@ -68,95 +68,97 @@ export default function EditProfileForm({ user }: { user: User | null }) {
   }
 
   return (
-    <div className="flex h-screen max-w-sm min-w-sm flex-col mx-auto bg-background text-foreground items-center justify-between relative overflow-hidden">
-      <div className=" z-30 flex flex-row justify-end items-center gap-1 p-2 fixed top-0 ">
+    <div className="min-h-screen max-w-sm min-w-sm flex-col mx-auto bg-background text-foreground relative">
+      <div className="z-30 flex flex-row justify-end items-center gap-1 p-2 fixed top-0 right-0 mr-4">
         <p className="text-foreground font-bold">{user?.tokens}</p>
         <CoinsIcon className="w-4 h-4 text-foreground" />
       </div>
-      <form
-        action={handleSubmit}
-        className="relative z-10 flex flex-col items-center gap-6 w-full max-w-sm min-w-sm px-8 py-10 rounded-3xl bg-card/70 pb-32"
-      >
-        <div className="inline-flex items-center border focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80 mb-4 rounded-full px-4 py-1.5 text-sm font-medium shadow-sm transition-none mt-8"><span className="mr-1 text-primary">✦</span>Edit your profile</div>
-        {user?.image && (
-          <div className="mb-2 flex justify-center w-full">
-            <Image
-              src={user.image}
-              alt="Profile"
-              width={96}
-              height={96}
-              className="rounded-full object-cover border-4 border-primary"
+      <div className="pt-12 pb-24 px-4 overflow-y-auto">
+        <form
+          action={handleSubmit}
+          className="flex flex-col items-center gap-6 w-full max-w-sm min-w-sm px-4 py-6 rounded-3xl bg-card/70"
+        >
+          <div className="inline-flex items-center border focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80 mb-4 rounded-full px-4 py-1.5 text-sm font-medium shadow-sm transition-none mt-8"><span className="mr-1 text-primary">✦</span>Edit your profile</div>
+          {user?.image && (
+            <div className="mb-2 flex justify-center w-full">
+              <Image
+                src={user.image}
+                alt="Profile"
+                width={96}
+                height={96}
+                className="rounded-full object-cover border-4 border-primary"
+              />
+            </div>
+          )}
+          <div className="flex flex-col gap-2 w-full">
+            <Label htmlFor="name" className="text-card-foreground">Name</Label>
+            <Input
+              id="name"
+              type="text"
+              placeholder="Name"
+              name="name"
+              defaultValue={user?.name || ""}
+              className="bg-muted text-muted-foreground border-none focus:ring-2 focus:ring-ring rounded-lg"
             />
           </div>
-        )}
-        <div className="flex flex-col gap-2 w-full">
-          <Label htmlFor="name" className="text-card-foreground">Name</Label>
-          <Input
-            id="name"
-            type="text"
-            placeholder="Name"
-            name="name"
-            defaultValue={user?.name || ""}
-            className="bg-muted text-muted-foreground border-none focus:ring-2 focus:ring-ring rounded-lg"
-          />
-        </div>
-        <div className="flex flex-col gap-2 w-full">
-          <Label htmlFor="bio" className="text-card-foreground">Bio</Label>
-          <Textarea
-            id="bio"
-            placeholder="Bio"
-            name="bio"
-            defaultValue={user?.bio || ""}
-            className="bg-muted text-muted-foreground border-none focus:ring-2 focus:ring-ring rounded-lg"
-          />
-        </div>
-        <div className="flex flex-col gap-2 w-full">
-          <Label htmlFor="image" className="text-card-foreground">Image</Label>
-          <Input
-            id="image"
-            type="file"
-            accept="image/*"
-            name="image"
-            className="bg-muted text-muted-foreground border-none focus:ring-2 focus:ring-ring rounded-lg file:bg-primary file:text-primary-foreground file:font-bold file:rounded file:px-3 file:py-1"
-          />
-        </div>
-        <div className="flex flex-row gap-2 w-full">
-          <Label htmlFor="gender" className="text-card-foreground">Gender</Label>
-          <Select
-            name="gender"
-            defaultValue={user?.gender || "MALE"}
+          <div className="flex flex-col gap-2 w-full">
+            <Label htmlFor="bio" className="text-card-foreground">Bio</Label>
+            <Textarea
+              id="bio"
+              placeholder="Bio"
+              name="bio"
+              defaultValue={user?.bio || ""}
+              className="bg-muted text-muted-foreground border-none focus:ring-2 focus:ring-ring rounded-lg"
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full">
+            <Label htmlFor="image" className="text-card-foreground">Image</Label>
+            <Input
+              id="image"
+              type="file"
+              accept="image/*"
+              name="image"
+              className="bg-muted text-muted-foreground border-none focus:ring-2 focus:ring-ring rounded-lg file:bg-primary file:text-primary-foreground file:font-bold file:rounded file:px-3 file:py-1"
+            />
+          </div>
+          <div className="flex flex-row gap-2 w-full">
+            <Label htmlFor="gender" className="text-card-foreground">Gender</Label>
+            <Select
+              name="gender"
+              defaultValue={user?.gender || "MALE"}
+            >
+              <SelectTrigger className="bg-muted text-muted-foreground border-none focus:ring-2 focus:ring-ring rounded-lg">
+                <SelectValue placeholder="Select a gender" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="MALE">Male</SelectItem>
+                <SelectItem value="FEMALE">Female</SelectItem>
+              </SelectContent>
+            </Select>
+            <Label htmlFor="lookingFor" className="text-card-foreground">Looking For</Label>
+            <Select
+              name="lookingFor"
+              defaultValue={user?.lookingFor || "FEMALE"}
+            >
+              <SelectTrigger className="bg-muted text-muted-foreground border-none focus:ring-2 focus:ring-ring rounded-lg">
+                <SelectValue placeholder="Select a gender" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="MALE">Male</SelectItem>
+                <SelectItem value="FEMALE">Female</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <WalletDisconnectButton style={{ width: "100%", textAlign: "center", justifyContent: "center" }} onClick={() => router.push('/')} />
+          <Button
+            className="w-full mt-2 bg-primary text-primary-foreground font-bold text-lg rounded-sm py-6 hover:bg-primary/80 transition-colors"
+            type="submit"
+            disabled={loading}
           >
-            <SelectTrigger className="bg-muted text-muted-foreground border-none focus:ring-2 focus:ring-ring rounded-lg">
-              <SelectValue placeholder="Select a gender" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="MALE">Male</SelectItem>
-              <SelectItem value="FEMALE">Female</SelectItem>
-            </SelectContent>
-          </Select>
-          <Label htmlFor="lookingFor" className="text-card-foreground">Looking For</Label>
-          <Select
-            name="lookingFor"
-            defaultValue={user?.lookingFor || "FEMALE"}
-          >
-            <SelectTrigger className="bg-muted text-muted-foreground border-none focus:ring-2 focus:ring-ring rounded-lg">
-              <SelectValue placeholder="Select a gender" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="MALE">Male</SelectItem>
-              <SelectItem value="FEMALE">Female</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        <WalletDisconnectButton style={{ width: "100%", textAlign: "center", justifyContent: "center" }} onClick={() => router.push('/')} />
-        <Button
-          className="w-full mt-2 bg-primary text-primary-foreground font-bold text-lg rounded-sm py-6 hover:bg-primary/80 transition-colors"
-          type="submit"
-          disabled={loading}
-        >
-          {loading ? "Saving..." : "Save"}
-        </Button>
-      </form>
+            {loading ? "Saving..." : "Save"}
+          </Button>
+        </form>
+      </div>
       <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-sm min-w-sm z-20 h-[10vh]">
         <Navbar user={user} publicKey={publicKey} />
       </div>
